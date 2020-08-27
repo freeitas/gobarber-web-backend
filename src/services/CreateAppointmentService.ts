a52e27c7de53df32cf141c951464c2f4c0d02d22
Guilherme Freitas <hfdsguilherme@gmail.com>
2020-08-27 18:56:17 -0300

    Cadastro de Usuario

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -5,12 +5,12 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   data: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ data, provider }: Request): Promise<Appointment> {
+  public async execute({ data, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(data);
@@ -24,7 +24,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
